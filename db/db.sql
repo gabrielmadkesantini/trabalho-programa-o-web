6dd474080ee4e51377f59333c27b28b41aaab871
Lucas Zorrer <lucaszorrer10@outlook.com>
2023-05-04 09:10:23 -0300

    Adicionando sql na pasta db

---
+++ db/db.sql
@@ -0,0 +1,17 @@
+CREATE SCHEMA IF NOT EXISTS `pw` DEFAULT CHARACTER SET utf8;
+
+CREATE TABLE IF NOT EXISTS `pw`.`users` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(200) NOT NULL,
+    `token` VARCHAR(1000) NULL,
+    PRIMARY KEY (`id`)
+);
+CREATE TABLE IF NOT EXISTS `pw`.`documents` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `path` VARCHAR(2000) NOT NULL,
+    `users_id` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY(`users_id`) REFERENCES users(id)
+)
