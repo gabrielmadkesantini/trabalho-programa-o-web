7cbb2dea6bfc302523b4271840539b06af1c96a1
Gabriel Santini <gabriel.111104@bento.local>
2023-05-05 11:16:31 -0300

    add more controllers

--- controller/create_produc.php
+++ controller/create_produc.php
@@ -1,18 +1,17 @@
 <?php
 
-require("twig_config");
 require("../model/Users.php");
 
-$nome = $_POST['nome'];
-$valor = $_POST['valor'];
-$quantia = $_POST['quantia'];
+// $nome = $_POST['nome'];
+// $valor = $_POST['valor'];
+// $quantia = $_POST['quantia'];
 
 $usr = new Users();
-
-$usr->create([
-"name"=> $nome,
-"email"=> $email,
-"password"=> $password
+$result = $usr->get_one([
+    "id"=>1,
 ]);
+
+var_dump($result);  
+
 
  
--- db/db.sql
+++ db/db.sql
@@ -1,16 +1,17 @@
-CREATE SCHEMA IF NOT EXISTS `pw` DEFAULT CHARACTER SET utf8;
+    CREATE SCHEMA IF NOT EXISTS `pw` DEFAULT CHARACTER SET utf8;
 
-CREATE TABLE IF NOT EXISTS `pw`.`users` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(100) NOT NULL,
-    `email` VARCHAR(100) NOT NULL,
-    `password` VARCHAR(200) NOT NULL,
-    PRIMARY KEY (`id`)
-);
-CREATE TABLE IF NOT EXISTS `pw`.`documents` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `path` VARCHAR(2000) NOT NULL,
-    `users_id` INT NOT NULL,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY(`users_id`) REFERENCES users(id)
-)
+    CREATE TABLE IF NOT EXISTS `pw`.`users` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `name` VARCHAR(100) NOT NULL,
+        `email` VARCHAR(100) NOT NULL,
+        `password` VARCHAR(200) NOT NULL,
+        `ativo` BOOLEAN NOT NULL DEFAULT 1,
+        PRIMARY KEY (`id`)
+    );
+    CREATE TABLE IF NOT EXISTS `pw`.`documents` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `path` VARCHAR(2000) NOT NULL,
+        `users_id` INT NOT NULL,
+        PRIMARY KEY (`id`),
+        FOREIGN KEY(`users_id`) REFERENCES users(id)
+    )
