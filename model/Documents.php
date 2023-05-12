4e112cc27c234c6a9907c9ac2ed05aa1f9f345bb
Gabriel Santini <gabriel.111104@bento.local>
2023-05-12 11:04:00 -0300

    All (is suposed) have been done

--- model/Documents.php
+++ model/Documents.php
@@ -7,22 +7,43 @@ class Documents extends Model
 {
   public function get_user_docs($id)
   {
-    $sql = $this->conex->prepare("SELECT * FROM {$this->table} WHERE ATIVO=1 AND USERID=$id");
-    $sql->execute($id);
+    $sql = $this->conex->prepare("SELECT * FROM {$this->table} WHERE ATIVO=1 AND USERID=:id");
+    $sql->execute([':id' => $id]);
 
     $response = $sql->fetchAll(PDO::FETCH_ASSOC);
     return $response;
   }
 
-  public function get_all_docs()
+  public function get_all_docs($id)
   {
-    $sql = $this->conex->prepare("SELECT * FROM {$this->table} WHERE ATIVO=1");
-    $sql->execute();
+
+    $and = 0;
+    if ($id) {
+      $and = "and permissions.user_id=:id";
+    } else {
+      $and = "";
+    }
+
+    $sql = $this->conex->prepare("SELECT documents.path, users.name, users.email FROM documents
+    JOIN permissions on permissions.document_id = documents.id 
+    JOIN users on documents.users_id = users.id where ativo=1 " . $and);
+    if ($id) {
+      $sql->execute([':id' => $id]);
+    } else {
+      $sql->execute();
+    }
+    
     $response = $sql->fetchAll(PDO::FETCH_ASSOC);
     return $response;
   }
 
-  public function get_filtred($data){
-$sql = $this->conex->prepare("SELECT * FROM {$this->table} WHERE ATIVO=1 and ");
+  public function permission($data)
+  {
+
+    $sql = $this->conex->prepare("UPDATE {$this->table} SET permission = 1 WHERE ATIVO=1 AND userId=:id");
+    $sql->bindParams(":id", $data);
+    $sql->execute();
   }
+
+
 }
